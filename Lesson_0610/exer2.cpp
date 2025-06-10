d20d2cddb6274725e7fa15c521f2cb51e35f0cc2
angwangsub <angwangsub@gmail.com>
2025-06-11 08:39:06 +0900

    회사Update

--- Lesson_0610/exer2.cpp
+++ Lesson_0610/exer2.cpp
@@ -1,11 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS   // scanf 함수 에러 무시  
 #include <stdio.h> // 표준 입출력에 관련된 함수들이 정의된 헤더
-#include <conio.h>  // Windows OS전용 입출력 관련 함수 정의 헤더
 #include <string.h>  // 문자열 관련 함수 정의 헤더
 
 // 숙제2 : 다음과 같은 형식으로 출력되는 함수를  만드세요 
 // char sz[] = "abcde";
-
 /*
 abcde
 bcde
@@ -16,16 +14,15 @@ e
 void print_char(char* ptr, int count)
 {
 	int i;
-	for (i = 0;i < count;i++)
+	for (i = 0; i < count; i++)
 	{
-		printf("%s\n", ptr+i );
+		printf("%s\n", ptr + i);
 	}
 }
-
 int main()
 {
-	char sz[] = "abcde"; 
-	int count = sizeof(sz) / sizeof(char);
-	print_char(sz, count-1);
+	char sz[] = "abcde";
+	print_char(sz, strlen(sz));
+
 	return 0;
 }
